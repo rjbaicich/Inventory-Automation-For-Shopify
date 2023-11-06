ee74b593d2b988cf6de957e60658b996b541c6d7
rjbaicich <rjbaicich@gmail.com>
2023-11-06 13:29:25 -0500

    update

---
+++ database.py
@@ -0,0 +1,12 @@
+import psycopg2
+import pandas as pd
+
+def create_inventory_table():
+    # PostgreSQL database operations for creating the inventory table
+
+def insert_data_into_inventory(data):
+    # Insert data into the inventory table
+
+if __name__ == "__main__":
+    create_inventory_table()
+    # Read data using Pandas and insert it into the database
