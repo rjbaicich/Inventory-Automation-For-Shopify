4ea52a339e2a1377f44f3dd4b929dfd09e5d92cc
rjbaicich <124328912+rjbaicich@users.noreply.github.com>
2023-11-14 11:14:10 -0500

    update

--- email.py
+++ email.py
@@ -1,9 +1,11 @@
 import imaplib
 import email
 import pandas as pd
+from email.header import decode_header
 
 def process_inventory_email():
-    # Email processing code here
-
-if __name__ == "__main__":
-    process_inventory_email()
+    # Email credentials and server details
+    email_address = 'your_email@example.com'
+    email_password = 'your_email_password'
+    imap_server = 'imap.example.com'
+    imap_port = 993  # Use the appropriate port for your email provider
