8d459949c36da00814f0e460c6e165c327219f7a
rjbaicich <124328912+rjbaicich@users.noreply.github.com>
2023-11-14 11:15:00 -0500

    update

--- email.py
+++ email.py
@@ -9,3 +9,13 @@ def process_inventory_email():
     email_password = 'your_email_password'
     imap_server = 'imap.example.com'
     imap_port = 993  # Use the appropriate port for your email provider
+   # Connect to the IMAP server
+    mail = imaplib.IMAP4_SSL(imap_server, imap_port)
+    mail.login(email_address, email_password)
+
+    # Select the mailbox (inbox in this case)
+    mail.select("inbox")
+
+    # Search for all emails in the mailbox
+    status, messages = mail.search(None, "ALL")
+    message_ids = messages[0].split()
