935a83bf5ff5457bab83d08f313d1a2aa23d8992
rjbaicich <124328912+rjbaicich@users.noreply.github.com>
2023-11-20 02:18:59 -0500

    Update

--- transform.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def transform_data(data):
-    # Data transformation operations
-
-if __name__ == "__main__":
-    data = pd.read_csv('inventory.csv')
-    transformed_data = transform_data(data)
-    transformed_data.to_csv('transformed_inventory.csv', index=False)
