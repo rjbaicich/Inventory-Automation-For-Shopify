1a0712d49caeb64d984d1184be472492347398ab
rjbaicich <124328912+rjbaicich@users.noreply.github.com>
2023-11-27 22:54:13 -0500

    update

--- email.py
+++ email.py
@@ -1,63 +1,16 @@
 import imaplib
 import email
-import pandas as pd
-from email.header import decode_header
-
-def process_inventory_email():
-    # Email credentials and server details
-    email_address = 'your_email@example.com'
-    email_password = 'your_email_password'
-    imap_server = 'imap.example.com'
-    imap_port = 993  # Use the appropriate port for your email provider
-   # Connect to the IMAP server
-    mail = imaplib.IMAP4_SSL(imap_server, imap_port)
-    mail.login(email_address, email_password)
-
-    # Select the mailbox (inbox in this case)
-    mail.select("inbox")
-
-    # Search for all emails in the mailbox
-    status, messages = mail.search(None, "ALL")
-    message_ids = messages[0].split()
-        # Process each email
-    for msg_id in message_ids:
-        # Fetch the email by ID
-        status, msg_data = mail.fetch(msg_id, "(RFC822)")
-        raw_email = msg_data[0][1]
-        
-        # Parse the raw email using the email library
-        msg = email.message_from_bytes(raw_email)
-        
-        # Extract information from the email (customize as needed)
-        subject, encoding = decode_header(msg["Subject"])[0]
-        subject = subject.decode(encoding) if isinstance(subject, bytes) else subject
-        
-        sender, encoding = decode_header(msg.get("From"))[0]
-        sender = sender.decode(encoding) if isinstance(sender, bytes) else sender
-        
-        # Additional information extraction based on your needs
-             
-        # For example, print the subject and sender
-        print(f"Subject: {subject}")
-        print(f"From: {sender}")
-
-        # Process the email content as needed (customize this part)
-        if msg.is_multipart():
-            for part in msg.walk():
-                content_type = part.get_content_type()
-                content_disposition = str(part.get("Content-Disposition"))
-
-                if "attachment" in content_disposition:
-                    # Handle attachments if needed
-                    pass
-                else:
-                    # Extract and process text content
-                    body = part.get_payload(decode=True)
-                    print(body.decode("utf-8"))
+import os
+from dotenv import load_dotenv
+from shopify import Shopify
 
 
-    # Close the connection
-    mail.logout()
+# Email Configuration
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+SENDER_EMAIL = "specific_sender@example.com"  # Replace with the specific sender's email
 
-if __name__ == "__main__":
-    process_inventory_email()
+# Connect to the email server
+mail = imaplib.IMAP4_SSL(EMAIL_HOST)
+mail.login(EMAIL_USER, EMAIL_PASSWORD)
