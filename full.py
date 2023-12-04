2919e441afef4b36917294a01b7cb8fc5bb95c7f
rjbaicich <124328912+rjbaicich@users.noreply.github.com>
2023-12-04 01:14:02 -0500

    update

--- full.py
+++ full.py
@@ -14,3 +14,17 @@ def fetch_and_upload_csv():
     # Connect to the email server
     mail = imaplib.IMAP4_SSL(EMAIL_HOST)
     mail.login(EMAIL_USER, EMAIL_PASSWORD)
+
+    # Select the mailbox you want to search (e.g., 'inbox')
+    mail.select("inbox")
+
+    # Search for emails from the specific sender
+    result, data = mail.search(None, f'(FROM "{SENDER_EMAIL}")')
+
+    # Get the latest email (assuming the emails are sorted by date)
+    latest_email_id = data[0].split()[-1]
+    result, message_data = mail.fetch(latest_email_id, "(RFC822)")
+
+    # Parse the email content
+    raw_email = message_data[0][1]
+    email_message = email.message_from_bytes(raw_email)
