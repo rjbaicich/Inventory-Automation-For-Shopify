06194e16c1b224cd254b94e9ab4746ad55599d33
rjbaicich <124328912+rjbaicich@users.noreply.github.com>
2023-12-11 02:35:22 -0500

    update4

--- AUTOMATER.py
+++ AUTOMATER.py
@@ -12,3 +12,11 @@ def convert_to_shopify_format(input_file, output_file="Inventory_update.csv"):
         'Vendor': 'Vendor',
         # Add more mappings as needed
     }
+      # Apply the mapping to the DataFrame
+    df = df.rename(columns=mapping)
+
+    # Additional formatting or data manipulation if needed
+
+    # Save the formatted DataFrame as a CSV file
+    df.to_csv(output_file, index=False)
+    print(f"Converted Shopify inventory saved as {output_file}")
