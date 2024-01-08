4aabec0140a24fbddb6801f292a1b1311179c804
rjbaicich <124328912+rjbaicich@users.noreply.github.com>
2024-01-08 01:07:08 -0500

    ups1

--- AUTOMATER.py
+++ AUTOMATER.py
@@ -13,3 +13,14 @@ def convert_to_shopify_format(input_file, output_file="Inventory_update.csv"):
         'Status': 'Status',
         # Add more mappings as needed
     }
+
+      # Warn about unmapped columns
+    unmapped_columns = set(df.columns) - set(mapping.keys())
+    if unmapped_columns:
+        print(f"Warning: Some columns do not have mappings: {', '.join(unmapped_columns)}")
+
+    # Convert 'Variant Price' column to numeric
+    df['Variant Price'] = pd.to_numeric(df['Variant Price'], errors='coerce')
+
+    # Apply the mapping to the DataFrame
+    df = df.rename(columns=mapping)
